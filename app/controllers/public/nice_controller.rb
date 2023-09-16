7853b44b3a4e218785fa52a1d0853e5df910672c
EC2 Default User <ec2-user@ip-172-31-35-22.ec2.internal>
2023-09-16 08:04:37 +0000

    220230916

--- app/controllers/public/nice_controller.rb
+++ app/controllers/public/nice_controller.rb
@@ -7,5 +7,15 @@ class Public::NiceController < ApplicationController
     @nices = Nice.find(nices)
   end 
   def create
-   
+   nice = Nice.find(params[:nice_id])
+   nice = current_user.nices.new(nice_id: nice.id)
+   nice.save
+   redirect_to menu_path(params[:nice_id])
+  end 
+  def destroy
+    nice = Nice.fiind(params[:nice_id])
+    nice = current_user.nices.find_by(nice_id: nice.id)
+    nice.destroy
+    redirect_to menu_path(params[:nice_id])
+  end 
 end
